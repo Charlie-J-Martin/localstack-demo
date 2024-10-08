f938f9bb19657b0d0f07449087b6a42c77068d75
charlie-j-martin <charliejm7@gmail.com>
2024-10-08 14:08:18 +0100

    Update package names and version numbers

--- packages/trade-generator/src/main.ts
+++ packages/trade-generator/src/main.ts
@@ -5,7 +5,7 @@ const queueUrl =
   'https://localhost.localstack.cloud:4566/000000000000/localstack-demo-queue';
 
 const main = () => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 500; i++) {
     const message = generateTrades();
     sendMessageToSQS(queueUrl, message);
   }
